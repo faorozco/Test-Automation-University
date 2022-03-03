d00c23074069dd701ed56b421964987438982b42
Fabio Andrés <faorozco@utp.edu.co>
2022-03-03 08:57:56 -0500

    initial

---
+++ variables.py
@@ -0,0 +1,21 @@
+_cars = 23
+cars = 24
+
+CARS = 25
+
+number_of_cars = 23
+kind_of_cars = "Ferrari"
+
+print(_cars)
+print(cars)
+print(CARS)
+print(number_of_cars)
+
+name = "Janelle Jones"
+school = "Foundation Elementary School"
+print(name + school)
+print(name + " " + school)
+print(name + " works at " + school + ".")
+
+#python string.format()
+print("{} works at {}.".format(name, school))
